81c11a6923e65ae8d778bc7abcd7d57cd3eeef33
Arthur Frantz <124110042+ArthurSkl@users.noreply.github.com>
2026-08-13 13:56:50 -0400

    fix: ordena index.php - header() antes de qualquer output p/ redirects no login

--- index.php
+++ index.php
@@ -1,38 +1,34 @@
 <?php
 session_start();
- 
 
 require "./vendor/autoload.php";
-require("includes/main/main_index.php");
 
 use App\Entity\Usuario;
 
- 
+// usuario ja logado? vai direto para o painel
 if(isset($_SESSION['id_user'])){
     header("Location: pages/listar_recados.php");
+    exit();
 }
-    
 
 // usuario clicou no botao de login?
 if(isset($_POST['btn_sub'])){
-    // pegando email, matricula e senha 
+    // pegando email, matricula e senha
     if(isset($_POST['email'],$_POST['matricula'],$_POST['senha'])){
-        // esta setado 
+        // esta setado
         $objUser = new Usuario($_POST['matricula'],$_POST['email'],$_POST['senha']);
         if($objUser->logar()){
             $_SESSION['msg'] = 'logado com sucesso!';
             header("Location: pages/listar_recados.php");
+            exit();
         }else{
             // mensagem de usuario ou senha invalidos
             header("Location: index.php?erro=credenciais_incorretas");
             exit();
-        }   
-    }else{
-        // nao esta setado -> modal ou pop up, notificando que nao esta setado!
+        }
     }
 }
 
+require("includes/main/main_index.php");
 require("includes/footer/footer.php");
 ?>
-
-
